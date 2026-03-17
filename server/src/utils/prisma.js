046608f7e08be2c83837447cb738b79755e84772
JSlater16 <jared@ihthumanperformance.com>
2026-03-17 17:47:14 -0400

    Fix deploy config and package json

--- server/src/utils/prisma.js
+++ server/src/utils/prisma.js
@@ -2,4 +2,8 @@ const path = require("path");
 const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
 
-d
+dotenv.config({ path: path.resolve(__dirname, "..", "..", ".env") });
+
+const prisma = new PrismaClient();
+
+module.exports = { prisma };
